91c1309be6cce35ae2dfcf724ebea18251a6cf75
ancestor9 <ancestorcho30@gmail.com>
2023-12-25 03:55:57 +0900

    Add select_fruit.sh script

---
+++ plugins/shell/select_fruit.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+FRUIT=$1
+
+if [ "$FRUIT" == "APPLE" ]; then
+    echo "You selected Apple"
+
+elif [ "$FRUIT" == "ORANGE" ]; then
+    echo "You selected ORANGE"
+
+elif [ "$FRUIT" == "GRAPE" ]; then
+    echo "You selected GRAPE"
+
+else
+    echo "You selected other Fruit"
+
+fi
