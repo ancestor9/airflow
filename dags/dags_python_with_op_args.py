87fcd8de165ac40d2b1198e7189c86d67497e36b
ancestor9 <ancestorcho30@gmail.com>
2023-12-26 22:23:20 +0900

    org-args upload

--- dags/dags_python_with_op_args.py
+++ dags/dags_python_with_op_args.py
@@ -9,13 +9,13 @@ with DAG(
     dag_id="dags_python_with_op_args",
     schedule="30 6 * * *",
     start_date=pendulum.datetime(2023, 3, 1, tz="Asia/Seoul"),
-    catchup=False,
+    catchup=False
 ) as dag:
     
     regist_t1 = PythonOperator(
         task_id = 'regist_t1',
         python_callable = regist,
-        op_orgs = ['zosanggoo', 'man', 'kr', 'seoul']
+        op_args = ['zosanggoo', 'man', 'kr', 'seoul']
     )
     
     regist_t1
