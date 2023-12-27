7a3b277a213cc222230f084d3f5f37c075b32f62
ancestor9 <ancestorcho30@gmail.com>
2023-12-27 15:44:31 +0900

    dags_python_show_templates

--- dags/dags_python_show_templates.py
+++ dags/dags_python_show_templates.py
@@ -1,19 +1,18 @@
-import datetime
-import pendulum
 from airflow.models.dag import DAG
+import pendulum
+import datetime
 from airflow.decorators import task
 
-# copy from example_python_operator DAGS
 with DAG(
     dag_id="dags_python_show_templates",
     schedule="30 9 * * *",
-    start_date=pendulum.datetime(2023, 10, 1, tz="Asia/Seoul"),
+    start_date=pendulum.datetime(2023, 3, 10, tz="Asia/Seoul"),
     catchup=True
 ) as dag:
-
-    @task(task_id="python_task")
-    def show_templates(**kwargs):
-        from pprint import pprint
-        pprint(kwargs)
     
+    @task(task_id='python_task')
+    def show_templates(**kwargs):
+        from pprint import pprint 
+        pprint(kwargs)
+
     show_templates()
