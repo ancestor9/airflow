05f8241cccb2c75740ee43df6c92f14990c44a0a
ancestor9 <ancestorcho30@gmail.com>
2023-12-27 14:27:08 +0900

    dags_python_show_templates upload

---
+++ dags/dags_python_show_templates.py
@@ -0,0 +1,19 @@
+import datetime
+import pendulum
+from airflow.models.dag import DAG
+from airflow.decorators import task
+
+# copy from example_python_operator DAGS
+with DAG(
+    dag_id="dags_python_show_templates",
+    schedule="30 9 * * *",
+    start_date=pendulum.datetime(2023, 10, 1, tz="Asia/Seoul"),
+    catchup=True,
+):
+
+    @task(task_id="python_task")
+    def show_templates(**kwargs):
+        from pprint import pprint
+        print(Kwargs)
+    
+    show_templates
