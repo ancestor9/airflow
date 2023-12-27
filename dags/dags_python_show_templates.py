d912b1a6ee3841005185c81491f3c4618bb9f32b
ancestor9 <ancestorcho30@gmail.com>
2023-12-27 14:43:13 +0900

    dags_python_show_templates

--- dags/dags_python_show_templates.py
+++ dags/dags_python_show_templates.py
@@ -8,8 +8,8 @@ with DAG(
     dag_id="dags_python_show_templates",
     schedule="30 9 * * *",
     start_date=pendulum.datetime(2023, 10, 1, tz="Asia/Seoul"),
-    catchup=True,
-):
+    catchup=True
+) as dag:
 
     @task(task_id="python_task")
     def show_templates(**kwargs):
