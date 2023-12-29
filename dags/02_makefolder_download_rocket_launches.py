6db9c7df74f8c4364df03a421aef7b581afebadd
ancestor9 <ancestorcho30@gmail.com>
2023-12-29 18:41:53 +0900

    launch

--- dags/02_makefolder_download_rocket_launches.py
+++ dags/02_makefolder_download_rocket_launches.py
@@ -27,29 +27,5 @@ download_launches = BashOperator(
     dag=dag,
 )
 
-def _get_pictures():
-    # Download all pictures in launches.json
-    with open("/tmp/launches.json") as f:
-        launches = json.load(f)
-        image_urls = [launch["image"] for launch in launches["results"]]
-        for image_url in image_urls:
-            try:
-                response = requests.get(image_url)
-                image_filename = image_url.split("/")[-1]
-                target_file = f"/tmp/images/{image_filename}"
-                with open(target_file, "wb") as f:
-                    f.write(response.content)
-                print(f"Downloaded {image_url} to {target_file}")
-            except requests_exceptions.MissingSchema:
-                print(f"{image_url} appears to be an invalid URL.")
-            except requests_exceptions.ConnectionError:
-                print(f"Could not connect to {image_url}.")
-
-get_pictures = PythonOperator(
-    task_id="get_pictures", python_callable=_get_pictures, dag=dag
-)
-
-# Other tasks can be added as needed...
-
 # Set up the task dependencies
-create_tmp_folder >> download_launches >> get_pictures
+create_tmp_folder >> download_launches
