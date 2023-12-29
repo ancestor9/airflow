34fed43cd269e3d91f8740add6900952f5db6857
ancestor9 <ancestorcho30@gmail.com>
2023-12-29 21:02:43 +0900

    sdfs

--- dags/dags_01_umbrella_simple_down.py
+++ dags/dags_01_umbrella_simple_down.py
@@ -24,13 +24,13 @@ bash_t2 = BashOperator(
 # Create the tmp folder if it doesn't exist
 create_tmp_folder = BashOperator(
     task_id="create_tmp_folder",
-    bash_command="mkdir -p ./temporary",
+    bash_command="mkdir -p ./tmp",
     dag=dag,
 )
 
 download_launches = BashOperator(
     task_id="download_launches",
-    bash_command="curl -o ./temporary/launches.json -L 'https://ll.thespacedevs.com/2.0.0/launch/upcoming'",  # noqa: E501
+    bash_command="curl -o ./tmp/launches.json -L 'https://ll.thespacedevs.com/2.0.0/launch/upcoming'",  # noqa: E501
     dag=dag,
 )
 
