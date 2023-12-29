20539ba5c4415a3bc636788800de531f63095da6
ancestor9 <ancestorcho30@gmail.com>
2023-12-29 17:41:43 +0900

    launch

--- dags/02_download_rocket_launches.py
+++ dags/02_download_rocket_launches.py
@@ -1,90 +1,39 @@
 import json
+import pathlib
+import airflow.utils.dates
 import requests
 import requests.exceptions as requests_exceptions
 from airflow import DAG
 from airflow.operators.bash import BashOperator
 from airflow.operators.python import PythonOperator
-from airflow.utils.dates import days_ago
 
 dag = DAG(
     dag_id="download_rocket_launches",
     description="Download rocket pictures of recently launched rockets.",
-    start_date=days_ago(1),
+    start_date=airflow.utils.dates.days_ago(14),
     schedule_interval="@daily",
 )
 
 download_launches = BashOperator(
     task_id="download_launches",
-    bash_command="curl -o /home/ancestor9/airflow/tmp/launches.json -L 'https://ll.thespacedevs.com/2.0.0/launch/upcoming'",  # noqa: E501
+    bash_command="curl -o /tmp/launches.json -L 'https://ll.thespacedevs.com/2.0.0/launch/upcoming'",  # noqa: E501
     dag=dag,
 )
 
+
 def _get_pictures():
+    # Ensure directory exists
+    pathlib.Path("/tmp/images").mkdir(parents=True, exist_ok=True)
 
     # Download all pictures in launches.json
-    with open("/home/ancestor9/airflow/tmp/launches.json") as f:
+    with open("/tmp/launches.json") as f:
         launches = json.load(f)
         image_urls = [launch["image"] for launch in launches["results"]]
         for image_url in image_urls:
             try:
                 response = requests.get(image_url)
                 image_filename = image_url.split("/")[-1]
-                target_file = f"/home/ancestor9/airflow/tmp/images/{image_filename}"
-                with open(target_file, "wb") as f:
-                    f.write(response.content)
-                print(f"Downloaded {image_url} to {target_file}")
-            except requests_exceptions.MissingSchema:
-                print(f"{image_url} appears to be an invalid URL.")
-            except requests_exceptions.ConnectionError:
-                print(f"Could not connect to {image_url}.")
-
-get_pictures = PythonOperator(
-    task_id="get_pictures", python_callable=_get_pictures, dag=dag
-)
-
-notify = BashOperator(
-    task_id="notify",
-    bash_command='echo "There are now $(ls /home/ancestor9/airflow/tmp/images/ | wc -l) images."',
-    dag=dag,
-)
-
-download_launches >> get_pictures >> notify
-
-
-'''
-import json
-import pathlib
-import airflow.utils.dates
-import requests
-import requests.exceptions as requests_exceptions
-from airflow.models.dag import DAG
-from airflow.operators.bash import BashOperator
-from airflow.operators.python import PythonOperator
-
-dag = DAG(
-    dag_id="download_rocket_launches",
-    description="Download rocket pictures of recently launched rockets.",
-    start_date=airflow.utils.dates.days_ago(1),
-    schedule_interval="@daily",
-)
-
-download_launches = BashOperator(
-    task_id="download_launches",
-    bash_command="curl -o /home/ancestor9/airflow/tmp/launches.json -L 'https://ll.thespacedevs.com/2.0.0/launch/upcoming'",  # noqa: E501
-    dag=dag,
-)
-
-def _get_pictures():
-
-    # Download all pictures in launches.json
-    with open("/home/ancestor9/airflow/tmp/launches.json") as f:
-        launches = json.load(f)
-        image_urls = [launch["image"] for launch in launches["results"]]
-        for image_url in image_urls:
-            try:
-                response = requests.get(image_url)
-                image_filename = image_url.split("/")[-1]
-                target_file = f"/home/ancestor9/airflow/tmp/images/{image_filename}"
+                target_file = f"/tmp/images/{image_filename}"
                 with open(target_file, "wb") as f:
                     f.write(response.content)
                 print(f"Downloaded {image_url} to {target_file}")
@@ -100,9 +49,8 @@ get_pictures = PythonOperator(
 
 notify = BashOperator(
     task_id="notify",
-    bash_command='echo "There are now $(ls /home/ancestor9/airflow/tmp/images/ | wc -l) images."',
+    bash_command='echo "There are now $(ls /tmp/images/ | wc -l) images."',
     dag=dag,
 )
 
 download_launches >> get_pictures >> notify
-'''
