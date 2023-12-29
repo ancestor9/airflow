3f9785dce7aaa9a13e40deb5995ca9be718691fd
ancestor9 <ancestorcho30@gmail.com>
2023-12-29 14:22:58 +0900

    lauche

--- dags/02_download_rocket_launches.py
+++ dags/02_download_rocket_launches.py
@@ -4,7 +4,7 @@ import pathlib
 import airflow.utils.dates
 import requests
 import requests.exceptions as requests_exceptions
-from airflow import DAG
+from airflow.models.dag import DAG
 from airflow.operators.bash import BashOperator
 from airflow.operators.python import PythonOperator
 
