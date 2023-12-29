f2fc55de17ff09a878370fcb2c2dde7e3843a2ba
ancestor9 <ancestorcho30@gmail.com>
2023-12-29 18:46:25 +0900

    llll

--- dags/01_unbrella_down.py
+++ dags/01_unbrella_down.py
@@ -6,10 +6,13 @@ from airflow.operators.dummy import DummyOperator
 
 dag = DAG(
     dag_id="01_umbrella",
-    description="Umbrella example with DummyOperators.",
+    description="Umbrellagi example with DummyOperators.",
     start_date=airflow.utils.dates.days_ago(5),
     schedule_interval="@daily",
 )
 
 fetch_weather_forecast = DummyOperator(task_id="fetch_weather_forecast", dag=dag)
 fetch_sales_data = DummyOperator(task_id="fetch_sales_data", dag=dag)
+
+# Set dependencies between all tasks
+fetch_weather_forecast >> fetch_sales_data
