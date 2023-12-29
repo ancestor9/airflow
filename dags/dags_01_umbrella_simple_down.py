72f5dad65e75e35259f9dced7500b2c32158cee2
ancestor9 <ancestorcho30@gmail.com>
2023-12-29 19:54:22 +0900

    lll

--- dags/dags_01_umbrella_simple_down.py
+++ dags/dags_01_umbrella_simple_down.py
@@ -6,23 +6,26 @@ dag = DAG(
     dag_id="02_umbrella_simple_makedir",
     start_date=airflow.utils.dates.days_ago(5),
     schedule_interval="@daily",
-) as dag:   
+)
 
-    # [START howto_operator_bash]
-    bash_t1 = BashOperator(
-        task_id="bash_t1",
-        bash_command="echo who are you",
-    )
-    
-    bash_t2 = BashOperator(
-        task_id="bash_t2",
-        bash_command="echo $HOSTNAME",
-    )
-'''    
-    # Create the tmp folder if it doesn't exist
-    create_tmp_folder = BashOperator(
-        task_id="create_tmp_folder",
-        bash_command="mkdir -p /tmp"
-    )
-'''
-bash_t1 >> bash_t2 # >> create_tmp_folder
+# [START howto_operator_bash]
+bash_t1 = BashOperator(
+    task_id="bash_t1",
+    bash_command="echo who are you",
+    dag=dag,
+)
+
+bash_t2 = BashOperator(
+    task_id="bash_t2",
+    bash_command="echo $HOSTNAME",
+    dag=dag,
+)
+
+# Create the tmp folder if it doesn't exist
+create_tmp_folder = BashOperator(
+    task_id="create_tmp_folder",
+    bash_command="mkdir -p /tmp",
+    dag=dag,
+)
+
+bash_t1 >> bash_t2 >> create_tmp_folder
