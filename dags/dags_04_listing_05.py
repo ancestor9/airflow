419b3b8b2bda7c95aa6c17c82e911a6e53fb5673
ancestor9 <ancestorcho30@gmail.com>
2024-01-04 12:51:51 +0900

    sdfs

--- dags/dags_04_listing_05.py
+++ dags/dags_04_listing_05.py
@@ -12,6 +12,7 @@ dag = DAG(
 
 def _get_data(execution_date):
     year, month, day, hour, *_ = execution_date.timetuple()
+    hour -= 3
     url = (
         "https://dumps.wikimedia.org/other/pageviews/"
         f"{year}/{year}-{month:0>2}/pageviews-{year}{month:0>2}{day:0>2}-{hour:0>2}0000.gz"
